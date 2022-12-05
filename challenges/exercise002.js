68b0042add2f6766d8a5bd871e2d970a564b41bb
Ponti-D <spaediaz@gmail.com>
2022-12-05 21:40:48 +0000

    refector and removed comments exercise002.js

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,37 +1,29 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
-  
-  //to get array of fillings
   return  sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  let isPersonFromManchester = (person.city==='Manchester');
-  return isPersonFromManchester;
+  return (person.city==='Manchester');
 }
 
 export function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
-  // Your code here!
-  
-  //devided people with 40 (bus can hold), use ceil to round up any bits
+  if (people === undefined) throw new Error("people is required"); 
   return Math.ceil(people / 40);
 
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-  const numberOfSheeps = arr.filter(word => word ==='sheep'); //using filter method you can find the sheep
+  
+  const numberOfSheeps = arr.filter(word => word ==='sheep'); 
+
   return numberOfSheeps.length;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  //return the flag
+  
   return person.address.city==='Manchester';
 }
