dffa604508c46a5ff611bb6be99275220d5c47f4
Ponti-D <spaediaz@gmail.com>
2022-12-05 21:31:40 +0000

    refector and removed comments exercise001.js

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,24 +2,16 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
 
-  //take word first char..convert to cap then add the rest of word string to it
-  let capWord = word.charAt(0).toUpperCase() + word.substring(1, word.length);
-  return  capWord;
+  return  word.charAt(0).toUpperCase() + word.substring(1, word.length);
 
 }
 
 export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  
-  // Add your code here!
-  //take first char of each name, uppercase it then add then with .
-  let firstNameInitial =firstName[0].toUpperCase();
-  let lastNameInitial = lastName[0].toUpperCase();
  
-  return firstNameInitial + "." + lastNameInitial; 
+  return  firstName[0].toUpperCase() + "." + lastName[0].toUpperCase();
 
 }
 
@@ -27,129 +19,78 @@ export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
   
-  // Add your code here!
-
-  //set the pricewith VAT then calculate the VAT and add to it
-  let priceWithVAT=originalPrice;
-
-  if (vatRate !== 0 ){ 
-    //toFixed returns string and adds 0 to whole number - so need to parseto back to numeric type
-    priceWithVAT =  parseFloat(((vatRate / 100 * originalPrice)  + originalPrice).toFixed(2));
-
-  }
-  return  priceWithVAT ;
+  return  parseFloat(((vatRate / 100 * originalPrice)  + originalPrice).toFixed(2)); 
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
 
-  let priceWithDiscount=originalPrice;
-  if (reduction!== 0 ){ 
-      priceWithDiscount =   parseFloat(Math.abs((reduction / 100 * originalPrice)  - originalPrice).toFixed(2));
-  }
-  
-  return priceWithDiscount;
+  return parseFloat(Math.abs((reduction / 100 * originalPrice)  - originalPrice).toFixed(2));
 
 }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
-  let totalChars = str.length; //get the length
+
+  let totalChars = str.length;
     
-  //check if it's even or odd using % modulo
   if(totalChars % 2 == 0) {
-       //if it's even get 2 from middle (char 0 based)
-       let startAt = (totalChars/2)+1;       
-       return str.substring(startAt-2, startAt);
+    let startAt = (totalChars/2)+1;       
+    return str.substring(startAt-2, startAt);
   } 
-  else { //odd 
-      //if it's odd get the middle char
-    
-      let startAt =Math.round((totalChars/2))-1;
-    
-      return str.substring(startAt,startAt+1);
+  else {
+    let startAt =Math.round((totalChars/2))-1;
+    return str.substring(startAt,startAt+1);
+
   }
 }
 
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
-  //using built-in array methods split,reverse and join
-  let splitWords = word.split(""); //split words into array 
-
-  splitWords =splitWords.reverse(); //reverse it
-  splitWords = splitWords.join(""); //then join them together
-
-  return splitWords;
+  return word.split("").reverse().join("");
 
 }
 
 export function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
 
-  //go through each element of words, reverse it then assign to new array 
   let newWords = [];
-
-  for(let i = 0; i < words.length; i++){
-
-      let splitWord=words[i].split("");
-      let newWord = splitWord.reverse().join("");
-      
-      newWords.push(newWord);
-  }
+  words.forEach(element => {
+    let newWord = reverseWord(element);
+    newWords.push(newWord);
+  });
+   
   return newWords;
 
 }
 
 export function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
 
-  let jsonUsers = JSON.stringify(users); //make it string then parse it to get objects with properties
-  let newusers=JSON.parse(jsonUsers);
   let linuxCount=0;
    
-  newusers.forEach(user => {
-   // console.log(user.type);
-    if(user.type=="Linux") linuxCount++; //count linux users
-  });
+  users.forEach(user => {if(user.type=="Linux") linuxCount++; });
+
   return linuxCount;
 }
 
 export function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
-   
+  
   let meanScore = 0;
-  for(let i=0;i<scores.length;i++){
-    //loop the array scores add then and then devide the total with length of array 
-    meanScore+=scores[i];
-  }
-
+  scores.forEach(element => meanScore+=element);
+  
   return parseFloat((meanScore /= scores.length).toFixed(2));
 }
 
 export function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  // Add your code here!
-
-  //if n is devisible by 3 using % modulo opeation
-  if(n % 3 === 0 && n % 5 === 0){
-    return "fizzbuzz";
-  }
-  else if(n % 5 === 0){
-    return "buzz";
-  }
-  else if(n % 3 !== 0 && n % 5 !== 0 ){
-    return n;
-  }
-  else if(n % 3 === 0){
-    return "fizz";
-  }
+ 
+  if(n % 3 === 0 && n % 5 === 0) return "fizzbuzz";
+  if(n % 5 === 0)  return "buzz" ;
+  if(n % 3 !== 0 && n % 5 !== 0 ) return n;
+  if(n % 3 === 0) return "fizz"
    
 
 }
